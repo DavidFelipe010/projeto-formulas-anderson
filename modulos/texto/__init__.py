41b13f134ef816aa8e0c743d3d9d5ec977fc66c4
David Felipe <david.dfcsb9@gmail.com>
2026-04-17 14:57:05 -0300

    Add files via upload

---
+++ modulos/texto/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+def limpa_terminal() -> none:
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def titulo(txt: str) -> none:
+    texto = txt.strip()
+    linha = '=' * (len(texto) + 16)
+
+    print(f'''{linha}
+        {texto.title()}
+{linha}''')
+
+
+titulo('oii tudp bekmm mna')
